f6d498aed61f6985b5a1de6e9c084ac7d8e4ff2c
Cpp-Gleb <115859857+Cpp-Gleb@users.noreply.github.com>
2023-10-08 01:38:53 +0300

    Update Students.cpp

--- Students.cpp
+++ Students.cpp
@@ -1,67 +1,100 @@
 #include <iostream>
 #include <string>
-class student{
-private:	
-	string name;
-	int group;
-	int ses[5];
+using namespace std;
+
+class student
+{
+private:
+	string _name;
+	int _group;
+	int _ses[5];
 public:
 	string get_name();
-	void set_name( _name);
-	
+	void set_name(string name);
+
 	int get_group();
-	void set_group( _group);
-	
+	void set_group(int group);
+
 	int* get_ses();
 	void set_ses(int ses[5]);
 };
 
-string student::get_name(){
-	return  _name;
+string student::get_name()
+{
+	return _name;
+}
+void student::set_name(string name)
+{
+	_name = name;
 }
 
-string student::set_name(){
-	name = _name;
+int student::get_group()
+{
+	return _group;
+}
+void student::set_group(int group)
+{
+	_group = group;
 }
 
-int student::get_group(){
-	return  _group;
-}
-
-int student::set_group(){
-	group = _group;
-}
-int* student::get_ses(){
+int* student::get_ses()
+{
 	return _ses;
 }
-
-void student::set_ses(int ses[5]){
-	for(i = 0; i < 5; i ++) {
+void student::set_ses(int ses[5])
+{
+	for (int i = 0; i < 5; i++)
+	{
 		_ses[i] = ses[i];
 	}
 }
 
+
+
 int main() {
 	student students[10];
-	for(i = 0; i < 10; i++){
+	float sum[10];
+	for(int i = 0; i < 10; i++){
 		cout << "student" << i + 1 << endl;
 		cout << "Name: ";
-		string namee
+		string namee;
 		cin >> namee;
-		.set_name(namee);
+		students -> set_name(namee);
 		
 		cout << "Group: ";
-		int gr
+		int gr;
 		cin >> gr;
-		.set_group(gr);
+		students -> set_group(gr);
 		
 		cout << "SES: ";
-		int sss[5]
-		for(j = 0; j < 5; j++){
-			cin >> sss[j] >> " ";
-		}
-		.set_ses;
-		cout << endl << endl;
-	}
 
+		
+		int sss[5];
+		for(int j = 0; j < 5; j++){
+			cin >> sss[j];
+			sum[i] = sum[i] + sss[j];
+		}
+		sum[i] = sum[i] / 5;
+
+		students -> set_ses(sss);
+		cout << endl << endl;
+
+		
+
+	}
+	int a = 0;
+	for (int i = 0; i < 10; i++) {
+		if (sum[i] > 4.0) {
+			for (int j = 0; j < 10; j++) {
+				
+			}
+		}
+		else {
+			a++;
+		}
+
+	}
+	if (a == 10) {
+		cout << "No one student with result higher than 4";
+	}
 }
